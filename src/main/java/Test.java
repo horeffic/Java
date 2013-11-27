93a795c591c21f971814a0c6a1038e6ded2aab62
Didjo <horeffic1234@ukr.net>
2013-11-27 22:08:13 +0200

    masterTest

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yldo
+ * Date: 11/27/13
+ * Time: 10:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

270018340f4984afcd2e79b13f72c5adb4233040
Didjo <horeffic1234@ukr.net>
2013-11-30 16:32:34 +0200

    added socket support

--- src/main/java/TestA.java
+++ src/main/java/TestA.java
@@ -1,5 +1,8 @@
+import java.io.*;
+import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,11 +12,41 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 public class TestA {
-    public static void main(String[] args) {
-        String mawa = "dog";
+    public static void main(String[] args) throws IOException {
+        String socketInput = null;
+
         Map<String, String> map = new HashMap<String, String>();
         map.put("dog","собака");
-        System.out.println(map.size());
-        System.out.println(map.get(mawa));
+
+        Random random = new Random();
+        int chance = random.nextInt(map.size())+1;
+
+        String key ="";
+        String value = "123";
+
+
+        ServerSocket serverSocket = new ServerSocket();
+        InetAddress inetAddress = InetAddress.getByName("192.168.1.132");
+        SocketAddress socketAddress = new InetSocketAddress(inetAddress, 7777);
+        serverSocket.bind(socketAddress);
+        Socket socket = serverSocket.accept();
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        bufferedWriter.write(value);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        socketInput = bufferedReader.readLine();
+        System.out.println(socketInput);
+
+        if (key == value) {
+            bufferedWriter.write("nice");
+        } else {
+            bufferedWriter.write("wrong");
+        }
+        bufferedWriter.flush();
+        bufferedWriter.flush();
+        socket.close();
     }
 }
